ae3b0d5a2c38db6eedbb8e34f2e4b63cde7b241d
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-13 12:05:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+basic.showIcon(IconNames.No)
