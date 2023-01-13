5d84d345260d19df36be6846a7c851a2282998db
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-13 12:23:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Chunke Munke")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("GoodBye")
-})
-basic.showString("Wassup Gang")
-basic.showNumber(69)
+basic.showString("Hola")
+basic.showString("Soy")
+basic.showString("Dora")
